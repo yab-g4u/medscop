060f974f61fb2552c0955762e553800b1a101acd
Betha <140443069+yab-g4u@users.noreply.github.com>
2025-06-27 18:07:58 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
 
+import express from 'express';
+import cors from 'cors';
+import fundingRoutes from './routes/fundingRoutes';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/funding', fundingRoutes);
+
+export default app;
